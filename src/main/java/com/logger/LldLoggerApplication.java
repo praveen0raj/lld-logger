451e961a21c33f575adb80c065989b211240f196
praveen-raj <praveen.raj@mettl.com>
2022-01-22 19:14:43 +0530

    Initial commit

---
+++ src/main/java/com/logger/LldLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logger/LldLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
